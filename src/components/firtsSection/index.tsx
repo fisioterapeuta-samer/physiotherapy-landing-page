455d16594f9fc31759a25bc5fa14a7e387098a1d
HessDev <hesspearldev@gmail.com>
2024-06-13 15:11:56 -0300

    improvement accessability

--- src/components/firtsSection/index.tsx
+++ src/components/firtsSection/index.tsx
@@ -4,14 +4,14 @@ import styles from "./firstSection.module.css";
 function FirstSection() {
   return (
     <div className={styles.content}>
-      <h1 className={styles.title}>
+      <p className={styles.title}>
         O atendimento de excelência no tratamento de suas lesões.
-      </h1>
-      <h3 className={styles.subTitle}>
+      </p>
+      <p className={styles.subTitle}>
         Ninguém merece sentir dor, <br /> consulte nossos procedimentos e marque
         uma consulta com um profissional que atende às necessidades mais
         especiais.
-      </h3>
+      </p>
       <a
         href="https://api.whatsapp.com/send?phone=5521966305390&text=Ola, achei seu anuncio na internet, sou [ seu nome :D ], e gostaria de agendar uma avaliação."
         target="_blank"
