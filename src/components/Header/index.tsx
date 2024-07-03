09148360b0d93a9041ae6b3d82f2b3df17a4d8e8
ddaniel.c_nascimento <daniel.conceicao@personetics.com>
2024-07-03 19:47:03 -0300

    fixing id typo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -74,7 +74,7 @@ function Header() {
               Procedimentos
             </button>
           </a>
-          <a href="#diferencias">
+          <a href="#diferenciais">
             <button
               className={styles.buttonStyle}
               onClick={() => setOpenMobileNav(false)}
